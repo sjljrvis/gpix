add308af03c8f2f4a0836aae1fc88d89b4fbe16b
sjljrvis <sjlchougule@gmail.com>
2018-08-13 04:29:19 +0530

    added static file server to router

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"fmt"
 	router "github.com/sjljrvis/gpix/router"
 	"github.com/subosito/gotenv"
 	"log"
 	"net/http"
 	"os"
-	"text/template"
 )
 
 func init() {
 	gotenv.Load()
+	log.Print("Loaded env file")
 	log.Print("Server started at ", os.Getenv("PORT"))
-	fmt.Print("{{1}}")
 }
 
 func main() {
--- router/router.go
+++ router/router.go
@@ -3,11 +3,14 @@ package router
 import (
 	"github.com/gorilla/mux"
 	UserController "github.com/sjljrvis/gpix/controller/user"
+	"net/http"
 )
 
 // NewRouter is router pointer
 func NewRouter() *mux.Router {
+	fs := http.FileServer(http.Dir("./public/"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", UserController.HomePage).Methods("GET")
+	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", fs))
 	return r
 }
