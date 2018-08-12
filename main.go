54d1cfadebcd2f30652866d7d99001ac0f8e16f7
sjljrvis <sjlchougule@gmail.com>
2018-08-13 04:21:35 +0530

    added env parser

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"fmt"
 	router "github.com/sjljrvis/gpix/router"
+	"github.com/subosito/gotenv"
 	"log"
 	"net/http"
+	"os"
+	"text/template"
 )
 
 func init() {
-	log.Print("Server started")
+	gotenv.Load()
+	log.Print("Server started at ", os.Getenv("PORT"))
+	fmt.Print("{{1}}")
 }
 
 func main() {
 	r := router.NewRouter()
 	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
