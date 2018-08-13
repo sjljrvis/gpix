f1cbaaec1c867b6d28bc2d48dbdc1187215ac7b9
sjljrvis <sjlchougule@gmail.com>
2018-08-14 02:25:16 +0530

    added heroku build files

--- controller/user/controller.go
+++ controller/user/controller.go
@@ -2,7 +2,7 @@ package controller
 
 import (
 	helper "github.com/sjljrvis/gpix/helper"
-	UserModel "github.com/sjljrvis/gpix/models/user"
+	model "github.com/sjljrvis/gpix/models/user"
 	tools "github.com/sjljrvis/gpix/tools"
 	"log"
 	"net/http"
@@ -11,7 +11,7 @@ import (
 // HomePage controller
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	log.Print(tools.MongoDB)
-	var results []UserModel.User
+	var results []model.User
 	err := tools.MongoDB.C("user").Find(nil).All(&results)
 	if err != nil {
 		helper.RespondWithError(w, 200, "error in mongoDB ") // TODO: Do something about the error
